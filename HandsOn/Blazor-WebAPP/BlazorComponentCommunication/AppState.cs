e54b434a8d9fc67e53afa54424cb6a18b1a6c947
santuparsi <santuparsi@gmail.com>
2026-01-13 12:22:45 +0530

    commit113

---
+++ HandsOn/Blazor-WebAPP/BlazorComponentCommunication/AppState.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorComponentCommunication
+{
+    public class AppState
+    {
+        public string Message { get; private set; }
+
+        public event Action OnChange;
+
+        public void SetMessage(string message)
+        {
+            Message = message;
+            OnChange?.Invoke();
+        }
+    }
+
+}
